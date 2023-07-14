7a2653d4bd281f4dcf9b82ba9c77c815107b4a19
StoneyDSP <nathanjhood@googlemail.com>
2023-07-14 22:43:03 +0200

    Filtering pacman headers for a CMake-style build

---
+++ include/pacman/version.h
@@ -0,0 +1,15 @@
+// pacman v6.0.2.4cfaf53950c1e2bbef7262e2e9b608f4f5a280d5
+// Version control info
+
+#ifndef pacman_VERSION_H_INCLUDED
+#define pacman_VERSION_H_INCLUDED
+
+#define pacman_VERSION_MAJOR "6"
+#define pacman_VERSION_MINOR "0"
+#define pacman_VERSION_PATCH "2"
+#define pacman_VERSION_TWEAK "4cfaf53950c1e2bbef7262e2e9b608f4f5a280d5"
+#define pacman_VERSION "6.0.2.4cfaf53950c1e2bbef7262e2e9b608f4f5a280d5"
+#define pacman_REPOSITORY "https://github.com/StoneyDSP/msys2-pacman.git"
+
+#endif // pacman_VERSION_H_INCLUDED
+
