2cc52ac505f17f5df7a94738bc4e1b6c22d096fa
StoneyDSP <nathanjhood@googlemail.com>
2023-07-21 23:02:22 +0200

    Started 'pacman'

---
+++ pacman/include/pacman/version.h
@@ -0,0 +1,15 @@
+// pacman v6.0.2.2e2b3f16fbf86fd9d1e7ee0376fc94dedd99f3ba
+// Version control info
+
+#ifndef pacman_VERSION_H_INCLUDED
+#define pacman_VERSION_H_INCLUDED
+
+#define pacman_VERSION_MAJOR "6"
+#define pacman_VERSION_MINOR "0"
+#define pacman_VERSION_PATCH "2"
+#define pacman_VERSION_TWEAK "2e2b3f16fbf86fd9d1e7ee0376fc94dedd99f3ba"
+#define pacman_VERSION "6.0.2.2e2b3f16fbf86fd9d1e7ee0376fc94dedd99f3ba"
+#define pacman_REPOSITORY "https://github.com/StoneyDSP/msys2-pacman.git"
+
+#endif // pacman_VERSION_H_INCLUDED
+
