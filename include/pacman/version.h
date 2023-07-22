1e1fcf5fd5dfb9535504d7225739bd9f39f564d9
StoneyDSP <nathanjhood@googlemail.com>
2023-07-22 05:32:28 +0200

    Making way for 'libmakepkg' submodule

--- include/pacman/version.h
+++ include/pacman/version.h
@@ -1,4 +1,4 @@
-// pacman v6.0.2.5473e4218f1a3043c06b4ad7d1b6c7cbc49bb4ee
+// pacman v6.0.2.a9a9433e0bf62b44d35e0e716b91a966d3400e03
 // Version control info
 
 #ifndef pacman_VERSION_H_INCLUDED
@@ -7,8 +7,8 @@
 #define pacman_VERSION_MAJOR "6"
 #define pacman_VERSION_MINOR "0"
 #define pacman_VERSION_PATCH "2"
-#define pacman_VERSION_TWEAK "5473e4218f1a3043c06b4ad7d1b6c7cbc49bb4ee"
-#define pacman_VERSION "6.0.2.5473e4218f1a3043c06b4ad7d1b6c7cbc49bb4ee"
+#define pacman_VERSION_TWEAK "a9a9433e0bf62b44d35e0e716b91a966d3400e03"
+#define pacman_VERSION "6.0.2.a9a9433e0bf62b44d35e0e716b91a966d3400e03"
 #define pacman_REPOSITORY "https://github.com/StoneyDSP/msys2-pacman.git"
 
 #endif // pacman_VERSION_H_INCLUDED
